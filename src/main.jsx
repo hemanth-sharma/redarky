c2b5755480b1b29f2b9cfe5447487b3bf137defe
Hemant Sharma <hemantsharmafli7@gmail.com>
2026-09-11 19:32:33 +0530

    Vercel Analytics Fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { inject } from '@vercel/analytics'
+import { inject } from '@vercel/analytics'
 
-// if (import.meta.env.PROD) {
-//   inject();
-// }
+if (import.meta.env.PROD) {
+  inject();
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
